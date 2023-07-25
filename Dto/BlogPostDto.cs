8cf97cffc07a6c610a615e035ed6d95c0fb7e8c4
Amirhossein Daghigh Kia <amirhosseindk@hotmail.com>
2023-07-25 13:35:01 +0330

    id

--- Dto/BlogPostDto.cs
+++ Dto/BlogPostDto.cs
@@ -2,6 +2,7 @@
 {
     public class BlogPostDto
     {
+        public int Id { get; set; }
         public string Author { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
